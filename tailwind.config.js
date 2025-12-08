7ec2ffbf6e4d9f69738c6f8f35bd5264fd0b4394
Byron Smith <ByronSmith@gmail.com>
2025-12-08 21:02:45 +0800

    Tailwind & Resposive

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./client/**/*.{html,js,css}",
+    "./client/style/**/*.css",
+    "./server/**/*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
