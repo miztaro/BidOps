2491efe05bd2a96e960703a4999884ebd12b0b49
Miz <2244768@slu.edu.ph>
2025-12-17 16:29:24 +0800

    edited readme and backend

--- admin-server/database.js
+++ admin-server/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'bidops',
     waitForConnections: true,
     connectionLimit: 10,
--- server/config/database.php
+++ server/config/database.php
@@ -19,7 +19,7 @@ class Database {
     private $port = 3306;
     private $db_name = "bidops";
     private $username = "root";
-    private $password = "";
+    private $password = "root";
     public $conn;
 
     public function getConnection() {
